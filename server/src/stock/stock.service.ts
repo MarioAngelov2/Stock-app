2718b800e93ab792be9d4e0cbe86a898a8eeb074
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-05-24 14:35:06 +0300

    Update stock.service.ts

--- server/src/stock/stock.service.ts
+++ server/src/stock/stock.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class StockService {
-  getDataByTimeSlice(start: string, end: string): any {
+  getDataByTimeSlice(start: string, end: string): any[] {
     const mockData = [
       {
         id: 1,
