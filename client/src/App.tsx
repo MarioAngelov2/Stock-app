a504aa78fae87e1446e9a78c5d92b6acf9103357
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-05-25 20:59:45 +0300

    Create Footer component and add css style to it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Home price={20} />
+      <Home />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import '../style/Footer.css'
+
+export interface Props {
+}
+
+export function Footer () {
+  return (
+    <div className='footer-container'>
+      <div className='footer-info'>
+        <p>STOCK APP</p>
+      </div>
+    </div>
+  );
+}
