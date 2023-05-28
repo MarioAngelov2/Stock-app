9844c926793428aa56e89a930ad642cc9bebd827
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-05-28 19:26:02 +0300

    Relocation of the functional data about displaying in child component

--- client/src/components/StockMarket.tsx
+++ client/src/components/StockMarket.tsx
@@ -1,55 +1,36 @@
-import { useState, ChangeEvent } from "react";
-import { addDays, format } from "date-fns";
+import { useState } from "react";
 import "../style/StockMarket.css";
+import { getStocks } from "../services/requester";
+import { StockMarketData } from "./StockMarketData";
 
 export interface IAppProps {}
 
+interface StockItem {
+  id: number;
+  price: number;
+  name: string
+  timestamp: string;
+}
+
 export function StockMarket() {
-  const [selectStartingDate, setSelectStartingDate] = useState<Date | null>(
-    null
-  );
-  const [selectEndingDate, setSelectedEndingDate] = useState<Date | null>(null);
+  const [data, setData] = useState<any>([]);
 
-  const handleChangeStart = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputDateTime = new Date(event.target.value);
-    setSelectStartingDate(inputDateTime);
+  const handleDataQuery = (params: { start: string; end: string }) => {
+    getStocks(params).then((result) => {
+      setData(result);
+    });
   };
-
-  const handleChangeEnd = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputDateTime = new Date(event.target.value);
-    setSelectedEndingDate(inputDateTime);
-  };
-
-  const minDate = new Date();
-  const maxDate = addDays(minDate, 3);
 
   return (
     <div className="stockMarket-container">
       <div className="market-container">
         <h2>Select a time slice to see all the stock prices!</h2>
-        <div className="dateTime-container">
-          <div className="startingDate-container">
-            <label>Select a starting date</label>
-            <input
-              type="datetime-local"
-              min={format(minDate, "yyyy-MM-dd'T'HH:mm")}
-              max={format(maxDate, "yyyy-MM-dd'T'HH:mm")}
-              onChange={handleChangeStart}
-            />
-          </div>
-          <div className="endingDate-container">
-            <label>Selected an ending date.</label>
-            <input
-              type="datetime-local"
-              min={format(minDate, "yyyy-MM-dd'T'HH:mm")}
-              max={format(maxDate, "yyyy-MM-dd'T'HH:mm")}
-              onChange={handleChangeEnd}
-            />
-          </div>
-        </div>
-        <div className="button">
-          <button className="search-btn">Search</button>
-        </div>
+        <StockMarketData onQuery={handleDataQuery} />
+       {
+        data.map((item: StockItem) => (
+          <h1 key={item.id}>{item.timestamp}</h1>
+        ))
+       }
       </div>
     </div>
   );
