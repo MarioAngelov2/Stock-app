25ec9dcc1e81172500e5b3a348183d7f3c525541
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-05-28 19:23:14 +0300

    Create StockMarketData to handle incoming data and display it

---
+++ client/src/components/StockMarketData.tsx
@@ -0,0 +1,70 @@
+import { useState, ChangeEvent } from "react";
+import { addDays, format } from "date-fns";
+import { getStocks } from "../services/requester";
+
+import "../style/StockMarket.css";
+
+export interface IAppProps {}
+
+interface DateQueryProps {
+  onQuery: (params: { start: string; end: string }) => void;
+}
+
+export function StockMarketData({ onQuery }: DateQueryProps) {
+  const [selectStartingDate, setSelectStargingDate] = useState<Date | null>(
+    null
+  );
+  const [selectEndingDate, setSelectEndingDate] = useState<Date | null>(null);
+
+  const handleChangeStart = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputDateTime = new Date(event.target.value);
+    setSelectStargingDate(inputDateTime);
+  };
+
+  const handleChangeEnd = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputDateTime = new Date(event.target.value);
+    setSelectEndingDate(inputDateTime);
+  };
+
+  const handleQuery = () => {
+    const params = {
+      start: selectStartingDate?.toISOString() ?? "",
+      end: selectEndingDate?.toISOString() ?? "",
+    };
+
+    onQuery(params);
+  };
+
+  const minDate = new Date();
+  const maxDate = addDays(minDate, 3);
+
+  return (
+    <>
+      <div className="dateTime-container">
+        <div className="startingDate-container">  
+          <label>Select a starting date</label>
+          <input
+            type="datetime-local"
+            min={format(minDate, "yyyy-MM-dd'T'HH:mm")}
+            max={format(maxDate, "yyyy-MM-dd'T'HH:mm")}
+            onChange={handleChangeStart}
+          />
+        </div>
+        <div className="endingDate-container">
+          <label>Selected an ending date</label>
+          <input
+            type="datetime-local"
+            min={format(minDate, "yyyy-MM-dd'T'HH:mm")}
+            max={format(maxDate, "yyyy-MM-dd'T'HH:mm")}
+            onChange={handleChangeEnd}
+          />
+        </div>
+      </div>
+      <div className="button">
+        <button className="search-btn" onClick={handleQuery}>
+          Search
+        </button>
+      </div>
+    </>
+  );
+}
