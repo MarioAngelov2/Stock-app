01c75417256a5382a573a728ffc6b7190b5824e9
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-06-09 14:28:11 +0300

    DIsplay best price for buying, best price for selling

--- client/src/components/StockMarket.tsx
+++ client/src/components/StockMarket.tsx
@@ -13,23 +13,71 @@ interface StockItem {
   timestamp: string;
 }
 
+interface BestPrices {
+  name: string;
+  buyPrice: number;
+  sellPrice: number;
+  buyTime: string;
+  sellTime: string;
+}
+
 export function StockMarket() {
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<{
+    stockData: StockItem[];
+    bestPrices: BestPrices;
+  }>({
+    stockData: [],
+    bestPrices: {
+      name: "",
+      buyPrice: 0,
+      sellPrice: 0,
+      buyTime: "",
+      sellTime: "",
+    },
+  });
 
   const handleDataQuery = (params: { start: string; end: string }) => {
     getStocks(params).then((result) => {
-      setData(result.stockData);
+      setData(result);
     });
   };
+
+  const formattedDate = {
+    buyTime: new Date(data.bestPrices.buyTime).toLocaleString(),
+    sellTime: new Date(data.bestPrices.sellTime).toLocaleString()
+  }
+
+  const bestPrices = (
+    <div className="bestPrices-container">
+      <div className="buy">
+        <h2>
+          Best time to buy
+        </h2>
+        <h4>{data.bestPrices.name}</h4>
+        <p>
+          {formattedDate.buyTime} - ${data.bestPrices.buyPrice}
+        </p>
+      </div>
+      <div className="sell">
+        <h2>
+          Best time to sell
+        </h2>
+        <h4>{data.bestPrices.name}</h4>
+        <p>
+          {formattedDate.sellTime} - ${data.bestPrices.sellPrice}
+        </p>
+      </div>
+    </div>
+  );
 
   return (
     <div className="stockMarket-container">
       <div className="market-container">
         <h2>Select a time slice to see all the stock prices!</h2>
         <StockMarketData onQuery={handleDataQuery} />
-        {data.length > 0 ? (
+        {data.stockData.length > 0 ? (
           <div className="stockData-container">
-            <h3>STOCK PRICES</h3>
+            {bestPrices}
             <div className="stockList-container">
               <StockMarketChart data={data} />
             </div>
