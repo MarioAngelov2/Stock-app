06a0aafd611ac8eba4de6f4d3e325862d2326e73
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-06-12 15:39:33 +0300

    Update StockMarketChart.tsx

--- client/src/components/StockMarketChart.tsx
+++ client/src/components/StockMarketChart.tsx
@@ -8,47 +8,64 @@ import {
 } from "recharts";
 
 export interface Props {
-  data: any[];
+  data: { stockData: any[] };
 }
 
 export function StockMarketChart(props: Props) {
   const { data } = props;
 
-  const chartData = data.map((stock: any) => ({
+  const desiredDataPoints = 100;
+  const totalDataPoints = data.stockData.length;
+  const samplingRate = Math.ceil(totalDataPoints / desiredDataPoints);
+
+  const sampledData = data.stockData.filter((_, index) => {
+    return index % samplingRate === 0;
+  });
+
+  const chartData = sampledData.map((stock: any) => ({
     x: new Date(stock.timestamp).toLocaleString(),
     price: stock.price,
     name: stock.name,
   }));
 
+
+  // const chartData = data.stockData.map((stock: any) => ({
+  //   x: new Date(stock.timestamp).toLocaleString(),
+  //   price: stock.price,
+  //   name: stock.name,
+  // }));
+
   return (
     <div className="chart-container">
       <div className="chart-wrapper">
-        <AreaChart width={1100} height={600} data={chartData}>
-          <CartesianGrid stroke="#ccc" opacity="0.5" vertical={false} />
-          <YAxis
-            dataKey="price"
-            axisLine={false}
-            domain={["dataMin, 'dataMax"]}
-            tickCount={10}
-            tickLine={false}
-            tickFormatter={(number) => `$${number.toFixed(2)}`}
-          />
-          <XAxis
-            dataKey="x"
-            tickLine={false}
-            axisLine={false}
-            tickFormatter={(str): any => {
-              return new Date(str).toLocaleTimeString();
-            }}
-          />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="price"
-            stroke="#1398C0"
-            fill="#1398C0"
-          />
-        </AreaChart>
+        <div>
+          <AreaChart width={1200} height={600} data={chartData}>
+            <CartesianGrid stroke="#ccc" opacity="0.5" vertical={false} />
+            <YAxis
+              dataKey="price"
+              axisLine={false}
+              domain={["dataMin, 'dataMax"]}
+              tickCount={10}
+              tickLine={false}
+              tickFormatter={(number) => `$${number.toFixed(2)}`}
+            />
+            <XAxis
+              dataKey="x"
+              tickLine={false}
+              axisLine={false}
+              tickFormatter={(str): any => {
+                return new Date(str).toLocaleTimeString();
+              }}
+            />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="price"
+              stroke="#1398C0"
+              fill="#1398C0"
+            />
+          </AreaChart>
+        </div>
       </div>
     </div>
   );
