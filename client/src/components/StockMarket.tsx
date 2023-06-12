5a526d28625940212266eb67dd2bc4bc0b1eb242
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-06-12 16:29:56 +0300

    Separate the best prices and input field for filling funds in separate component

--- client/src/components/StockMarket.tsx
+++ client/src/components/StockMarket.tsx
@@ -3,8 +3,7 @@ import "../style/StockMarket.css";
 import { getStocks } from "../services/requester";
 import { StockMarketData } from "./StockMarketData";
 import { StockMarketChart } from "./StockMarketChart";
-
-export interface IAppProps {}
+import { StockMarketBestPrices } from "./StockMarketBestPrices";
 
 interface StockItem {
   id: number;
@@ -22,8 +21,6 @@ interface BestPrices {
 }
 
 export function StockMarket() {
-  const [funds, setFunds] = useState<any>(0);
-  const maxLength = 6;
 
   const [data, setData] = useState<{
     stockData: StockItem[];
@@ -44,65 +41,6 @@ export function StockMarket() {
       setData(result);
     });
   };
-  console.log(data)
-
-  const formattedDate = {
-    buyTime: new Date(data.bestPrices.buyTime).toLocaleString(),
-    sellTime: new Date(data.bestPrices.sellTime).toLocaleString(),
-  };
-
-  const bestPrices = (
-    <div className="bestPrices-container">
-      <div className="buy">
-        <h2>Best time to buy</h2>
-        <h4>{data.bestPrices.name}</h4>
-        <p>
-          {formattedDate.buyTime} - ${data.bestPrices.buyPrice}
-        </p>
-      </div>
-      <div className="sell">
-        <h2>Best time to sell</h2>
-        <h4>{data.bestPrices.name}</h4>
-        <p>
-          {formattedDate.sellTime} - ${data.bestPrices.sellPrice}
-        </p>
-      </div>
-    </div>
-  );
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    const onlyNumbers = value.replace(/[^0-9]/g, "");
-    setFunds(onlyNumbers);
-
-    if (value.length >= maxLength) {
-      alert('Maximum funds exceeded');
-    } 
-  };
-
-  const boughtStocks = (funds * data.bestPrices.buyPrice).toFixed(2);
-  const soldStocksPrice = (
-    Number(boughtStocks) * data.bestPrices.sellPrice
-  ).toFixed(2);
-  const profit = (Number(soldStocksPrice) - funds).toFixed(2);
-
-  const fundField = (
-    <div className="funds-container">
-      <input
-        placeholder="Enter your funds..."
-        onChange={handleChange}
-        // disabled={funds.length >= maxLength}
-      />
-      <div className="fundsResult">
-        <span>Buy date: {formattedDate.buyTime}</span>
-        <span>Sell date: {formattedDate.sellTime}</span>
-        <span>Bought stocks: ${boughtStocks}</span>
-        <span>Sold for: ${soldStocksPrice}</span>
-        <span>Profit: ${profit}</span>
-        <span></span>
-      </div>
-    </div>
-  );
 
   return (
     <div className="stockMarket-container">
@@ -111,8 +49,7 @@ export function StockMarket() {
         <StockMarketData onQuery={handleDataQuery} />
         {data.stockData.length > 0 ? (
           <div className="stockData-container">
-            {bestPrices}
-            {fundField}
+            <StockMarketBestPrices data={data}/>
             <div className="stockList-container">
               <StockMarketChart data={data} />
             </div>
---
+++ client/src/components/StockMarketBestPrices.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+export interface Props {
+  data: { bestPrices: any };
+}
+
+export function StockMarketBestPrices(props: Props) {
+  const { data } = props;
+
+  const [funds, setFunds] = useState<any>(0);
+  const maxLength = 6;
+
+  const formattedDate = {
+    buyTime: new Date(data.bestPrices.sellTime).toLocaleString(),
+    sellTime: new Date(data.bestPrices.buyTime).toLocaleString(),
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const onlyNumbers = value.replace(/[^0-9]/g, "");
+    setFunds(onlyNumbers);
+
+    if (value.length >= maxLength) {
+      alert("Maximum funds exceeded");
+    }
+  };
+
+  const boughtStocks = (funds * data.bestPrices.buyPrice).toFixed(2);
+  const soldStocksPrice = (
+    Number(boughtStocks) * data.bestPrices.sellPrice
+  ).toFixed(2);
+  const profit = (Number(soldStocksPrice) - funds).toFixed(2);
+
+  return (
+    <>
+      <div className="bestPrices-container">
+        <div className="buy">
+          <h2>Best time to buy</h2>
+          <h4>{data.bestPrices.name}</h4>
+          <p>
+            {formattedDate.buyTime} - ${data.bestPrices.buyPrice}
+          </p>
+        </div>
+        <div className="sell">
+          <h2>Best time to sell</h2>
+          <h4>{data.bestPrices.name}</h4>
+          <p>
+            {formattedDate.sellTime} - ${data.bestPrices.sellPrice}
+          </p>
+        </div>
+      </div>
+      <div className="funds-container">
+        <input
+          placeholder="Enter your funds..."
+          onChange={handleChange}
+        />
+        <div className="fundsResult">
+          <span>Buy date: {formattedDate.buyTime}</span>
+          <span>Sell date: {formattedDate.sellTime}</span>
+          <span>Bought stocks: ${boughtStocks}</span>
+          <span>Sold for: ${soldStocksPrice}</span>
+          <span>Profit: ${profit}</span>
+          <span></span>
+        </div>
+      </div>
+    </>
+  );
+}
