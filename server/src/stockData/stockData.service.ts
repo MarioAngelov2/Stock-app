4f65e2bde3308f6e236a687f6f5f48ce61a42d1a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-06-27 11:27:45 +0300

    Added worker thread

--- server/src/stockData/stockData.service.ts
+++ server/src/stockData/stockData.service.ts
@@ -17,35 +17,4 @@ export class StockDataService {
       this.isGeneratedData = true;
     }
   }
-
-  // const worker = new Worker('../workers/worker.ts');
-  // worker.on('message', (data) => {
-  //   console.log(data);
-  //   this.dataStore = data;
-  //   this.isGeneratedData = true;
-
-  // const newData = {};
-
-  // const endingDate = new Date();
-  // const startingDate = subDays(endingDate, 3);
-
-  // let currentTimeStamp = startingDate.getTime();
-
-  // while (currentTimeStamp <= endingDate.getTime()) {
-  //   let currentDate = new Date(currentTimeStamp);
-
-  //   let randomPrice = (1 + Math.random()).toFixed(2);
-
-  //   const newDataEntry = {
-  //     id: currentDate.toISOString(),
-  //     name: 'Tesla',
-  //     price: randomPrice,
-  //     timestamp: currentDate.toISOString(),
-  //   };
-
-  //   newData[currentDate.toISOString()] = newDataEntry;
-  //   currentTimeStamp += 1000;
-  // }
-
-  // this.dataStore = newData;
 }
