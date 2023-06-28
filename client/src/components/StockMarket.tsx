0bf6ab2f5f1f99b7ec44a18de8ed05398dc8e38f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-06-28 21:28:50 +0300

    Minor refactor

--- client/src/components/StockMarket.tsx
+++ client/src/components/StockMarket.tsx
@@ -6,57 +6,58 @@ import { StockMarketChart } from "./StockMarketChart";
 import { StockMarketBestPrices } from "./StockMarketBestPrices";
 
 interface StockItem {
-  id: number;
-  price: number;
-  name: string;
-  timestamp: string;
+    id: number;
+    price: number;
+    name: string;
+    timestamp: string;
 }
 
 interface BestPrices {
-  name: string;
-  buyPrice: number;
-  sellPrice: number;
-  buyTime: string;
-  sellTime: string;
+    name: string;
+    buyPrice: number;
+    sellPrice: number;
+    buyTime: string;
+    sellTime: string;
 }
 
 export function StockMarket() {
-  const [data, setData] = useState<{
-    stockData: StockItem[];
-    bestPrices: BestPrices;
-  }>({
-    stockData: [],
-    bestPrices: {
-      name: "",
-      buyPrice: 0,
-      sellPrice: 0,
-      buyTime: "",
-      sellTime: "",
-    },
-  });
-
-  const handleDataQuery = (params: { start: string; end: string }) => {
-    getStocks(params).then((result) => {
-      setData(result);
+    const [data, setData] = useState<{
+        stockData: StockItem[];
+        bestPrices: BestPrices;
+    }>({
+        stockData: [],
+        bestPrices: {
+            name: "",
+            buyPrice: 0,
+            sellPrice: 0,
+            buyTime: "",
+            sellTime: "",
+        },
     });
-  };
 
-  return (
-      <div className="stockMarket-container" id="stockMarket">
-        <div className="market-container">
-          <h2>Select a time slice to see all the stock prices!</h2>
-          <StockMarketData onQuery={handleDataQuery} />
-          {data.stockData.length > 0 ? (
-            <div className="stockData-container">
-              <StockMarketBestPrices data={data} />
-              <div className="stockList-container">
-                <StockMarketChart data={data} />
-              </div>
+    const handleDataQuery = (params: { start: string; end: string }) => {
+        getStocks(params).then((result) => {
+            setData(result);
+        });
+    };
+
+    return (
+        <div className="stockMarket-container" id="stockMarket">
+            <div className="market-container">
+                <h2>Select a time slice to see all the stock prices!</h2>
+                <StockMarketData onQuery={handleDataQuery} />
+
+                {data.stockData.length > 0 ? (
+                    <div className="stockData-container">
+                        <StockMarketBestPrices data={data} />
+                        <div className="stockList-container">
+                            <StockMarketChart data={data} />
+                        </div>
+                    </div>
+                ) : (
+                    <h3>No stock data available.</h3>
+                )}
             </div>
-          ) : (
-            <h3>No stock data available.</h3>
-          )}
         </div>
-      </div>
-  );
+    );
 }
--- client/src/components/StockMarketChart.tsx
+++ client/src/components/StockMarketChart.tsx
@@ -1,64 +1,100 @@
 import {
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  AreaChart,
-  Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    AreaChart,
+    Area,
 } from "recharts";
 
 export interface Props {
-  data: { stockData: any[] };
+    data: { stockData: any[] };
 }
 
 export function StockMarketChart(props: Props) {
-  const { data } = props;
+    const { data } = props;
 
-  const desiredDataPoints = 500;
-  const totalDataPoints = data.stockData.length;
-  const samplingRate = Math.ceil(totalDataPoints / desiredDataPoints);
+    const desiredDataPoints = 500;
+    const totalDataPoints = data.stockData.length;
+    const samplingRate = Math.ceil(totalDataPoints / desiredDataPoints);
 
-  const sampledData = data.stockData.filter((_, index) => {
-    return index % samplingRate === 0;
-  });
+    const sampledData = data.stockData.filter((_, index) => {
+        return index % samplingRate === 0;
+    });
 
-  
-  const chartData = sampledData.map((stock: any) => ({
-    time: new Date(stock.timestamp).toLocaleString(),
-    price: stock.price,
-    name: stock.name,
-  }));
+    const chartData = sampledData.map((stock: any) => ({
+        time: new Date(stock.timestamp).toLocaleString(),
+        price: stock.price,
+        name: stock.name,
+    }));
 
-  return (
-    <div className="chart-container">
-      <div className="chart-wrapper">
-        <div>
-          <AreaChart width={1200} height={600} data={chartData}>
-            <CartesianGrid stroke="#ccc" opacity="0.5" vertical={false} />
-            <YAxis
-              dataKey="price"
-              axisLine={false}
-              domain={["dataMin, 'dataMax"]}
-              tickCount={10}
-              tickLine={false}
-              tickFormatter={(number) => `$${number.toFixed(2)}`}
-            />
-            <XAxis
-              dataKey="time"
-              tickLine={false}
-              axisLine={false}
-              tickFormatter={(time): any => time}
-            />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="price"
-              stroke="#1398C0"
-              fill="#1398C0"
-            />
-          </AreaChart>
+    return (
+        <div className="chart-container">
+            <div className="chart-wrapper">
+                <div>
+                    <AreaChart width={1200} height={600} data={chartData}>
+                        <CartesianGrid
+                            stroke="#ccc"
+                            opacity="0.5"
+                            vertical={false}
+                        />
+                        <YAxis
+                            dataKey="price"
+                            axisLine={false}
+                            domain={["dataMin, 'dataMax"]}
+                            tickCount={10}
+                            tickLine={false}
+                            tickFormatter={(number) => `$${number.toFixed(2)}`}
+                        />
+                        <XAxis
+                            dataKey="time"
+                            tickLine={false}
+                            axisLine={false}
+                            tickFormatter={(time): any => time}
+                        />
+                        <Tooltip />
+                        <Area
+                            type="monotone"
+                            dataKey="price"
+                            stroke="#1398C0"
+                            fill="#1398C0"
+                        />
+                    </AreaChart>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
+}
+
+{
+    /* <div className="chart-container">
+<div className="chart-wrapper">
+  <div>
+    <AreaChart width={1200} height={600} data={chartData}>
+      <CartesianGrid stroke="#ccc" opacity="0.5" vertical={false} />
+      <YAxis
+        dataKey="price"
+        axisLine={false}
+        domain={["dataMin, 'dataMax"]}
+        tickCount={10}
+        tickLine={false}
+        tickFormatter={(number) => `$${number.toFixed(2)}`}
+      />
+      <XAxis
+        dataKey="time"
+        tickLine={false}
+        axisLine={false}
+        tickFormatter={(time): any => time}
+      />
+      <Tooltip />
+      <Area
+        type="monotone"
+        dataKey="price"
+        stroke="#1398C0"
+        fill="#1398C0"
+      />
+    </AreaChart>
+  </div>
+</div>
+</div>  */
 }
