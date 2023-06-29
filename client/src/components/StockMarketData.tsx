6b2c6f03da9fe5e78dc94d2863ce9d0f3eb91add
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-06-29 07:41:02 +0300

    Minor refactor

--- client/src/components/StockMarketData.tsx
+++ client/src/components/StockMarketData.tsx
@@ -6,75 +6,72 @@ import "../style/StockMarket.css";
 export interface IAppProps {}
 
 interface DateQueryProps {
-  onQuery: (params: { start: string; end: string }) => void;
+    onQuery: (params: { start: string; end: string }) => void;
 }
 
 export function StockMarketData({ onQuery }: DateQueryProps) {
+    const [selectStartingDate, setSelectStargingDate] = useState<Date | null>(
+        null
+    );
+    const [selectEndingDate, setSelectEndingDate] = useState<Date | null>(null);
 
-  const [selectStartingDate, setSelectStargingDate] = useState<Date | null>(
-    null
-  );
-  const [selectEndingDate, setSelectEndingDate] = useState<Date | null>(null);
-
-  const handleChangeStart = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputDateTime = new Date(event.target.value);
-    setSelectStargingDate(inputDateTime);
-  };
-
-  const handleChangeEnd = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputDateTime = new Date(event.target.value);
-    setSelectEndingDate(inputDateTime);
-  };
-
-  const handleQuery = () => {
-    const params = {
-      start: selectStartingDate?.toISOString() ?? "",
-      end: selectEndingDate?.toISOString() ?? "",
+    const handleChangeStart = (event: ChangeEvent<HTMLInputElement>) => {
+        const inputDateTime = new Date(event.target.value);
+        setSelectStargingDate(inputDateTime);
     };
 
-    if (!params.start && !params.end) {
-      alert("Select a valid date.");
-    } else if (!params.start || !params.end) {
-      alert("Select a valid date.");
-    }
+    const handleChangeEnd = (event: ChangeEvent<HTMLInputElement>) => {
+        const inputDateTime = new Date(event.target.value);
+        setSelectEndingDate(inputDateTime);
+    };
 
-    if (params.end < params.start) {
-      alert("Select a valid date.");
-    }
+    const handleQuery = () => {
+        const params = {
+            start: selectStartingDate?.toISOString() ?? "",
+            end: selectEndingDate?.toISOString() ?? "",
+        };
 
-    onQuery(params);
-  };
+        if (!params.start && !params.end) {
+            alert("Select a valid date.");
+        } else if (!params.start || !params.end) {
+            alert("Select a valid date.");
+        } else if (params.end < params.start) {
+            alert("Select a valid date.");
+        }
 
-  const currentDate = new Date();
-  const substractedDate = subDays(currentDate, 3);
+        onQuery(params);
+    };
 
-  return (
-    <>
-      <div className="dateTime-container">
-        <div className="startingDate-container">
-          <label>Select a starting date</label>
-          <input
-            type="datetime-local"
-            min={format(substractedDate, "yyyy-MM-dd'T'HH:mm")}
-            max={format(currentDate, "yyyy-MM-dd'T'HH:mm")}
-            onChange={handleChangeStart}
-          />
-        </div>
-        <div className="endingDate-container">
-          <label>Selected an ending date</label>
-          <input
-            type="datetime-local"
-            min={format(substractedDate, "yyyy-MM-dd'T'HH:mm")}
-            max={format(currentDate, "yyyy-MM-dd'T'HH:mm")}
-            onChange={handleChangeEnd}
-          />
-        </div>
-      </div>
-      <div className="button">
-        <button className="search-btn" onClick={handleQuery}>
-          Search
-        </button>
-      </div>
-    </>
-  );
+    const currentDate = new Date();
+    const substractedDate = subDays(currentDate, 3);
+
+    return (
+        <>
+            <div className="dateTime-container">
+                <div className="startingDate-container">
+                    <label>Select a starting date</label>
+                    <input
+                        type="datetime-local"
+                        min={format(substractedDate, "yyyy-MM-dd'T'HH:mm")}
+                        max={format(currentDate, "yyyy-MM-dd'T'HH:mm")}
+                        onChange={handleChangeStart}
+                    />
+                </div>
+                <div className="endingDate-container">
+                    <label>Selected an ending date</label>
+                    <input
+                        type="datetime-local"
+                        min={format(substractedDate, "yyyy-MM-dd'T'HH:mm")}
+                        max={format(currentDate, "yyyy-MM-dd'T'HH:mm")}
+                        onChange={handleChangeEnd}
+                    />
+                </div>
+            </div>
+            <div className="button">
+                <button className="search-btn" onClick={handleQuery}>
+                    Search
+                </button>
+            </div>
+        </>
+    );
 }
